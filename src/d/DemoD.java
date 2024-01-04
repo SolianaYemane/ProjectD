1918a50f87c73d323703eed3523583096006f2c8
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-03 19:38:28 -0800

    Update DemoD.java

--- src/d/DemoD.java
+++ src/d/DemoD.java
@@ -4,9 +4,10 @@ public class DemoD {
 	public static void main(String[] args) {
 		System.out.println("Thursday"); 
 		System.out.println("Sprint 4 done");  
+}
+	public void add(int a, int b){
+	   System.out.println(a+b); 
 
-		
-	}
-
+	} 
 }
  
